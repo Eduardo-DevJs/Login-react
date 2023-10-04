c5dbdf362ee29e9070a9cd7e09eed6688190bd94
Eduardo DevJS <dudev2001@gmail.com>
2023-10-04 00:45:25 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Container,
+  Link,
+  Paragrafo,
+  Body,
+  FormDiv,
+  Input,
+  Label,
+  H2,
+  Button,
+} from './style';
 
-function App() {
+const App = () => {
+  const textLogin = 'Seja bem vindo';
+  const textButton = 'Login';
+  const textPara = 'Nao tem conta?';
+  const textCreateLogin = 'Criar';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Body>
+      <Container>
+        <FormDiv>
+          <H2>{textLogin}</H2>
+          <Label for="nome">Nome</Label>
+          <Input id="nome" placeholder="Digite seu nome"></Input>
+          <Label for="senha">Senha</Label>
+          <Input type="password" placeholder="Digite seu nome"></Input>
+          <Button>{textButton}</Button>
+          <Paragrafo>{textPara} <Link>{textCreateLogin}</Link> </Paragrafo>
+        </FormDiv>
+      </Container>
+    </Body>
   );
-}
+};
 
 export default App;
---
+++ src/style.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const Body = styled.body`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #ddd;
+`;
+
+export const FormDiv = styled.div`
+  background-color: #fff;
+  padding: 40px;
+  border-radius: 8px;
+`;
+
+export const Label = styled.label `
+  font-size: 1rem;
+  display: block;
+  margin: 10px 0;
+`
+
+export const H2 = styled.h2 `
+  font-size: 1.2rem;
+  text-align: center;
+  margin-bottom: 20px;
+`
+
+export const Input = styled.input `
+  padding: .5rem .8rem;
+  outline: none;
+  font-size: 1rem;
+`
+
+export const Button = styled.button `
+  color: #fff;
+  margin-top: 15px;
+  background-color: #333;
+  cursor: pointer;
+  border-radius: 4px;
+  display: block;
+  width: 100%;
+  border: none;
+  padding: .8rem;
+`
+
+
+export const Paragrafo = styled.p `
+  color: #333;
+  text-align: center;
+  margin-top: 25px;
+`
+
+export const Link = styled.a `
+  color: blue;
+  margin-top: 15px;
+
+`
